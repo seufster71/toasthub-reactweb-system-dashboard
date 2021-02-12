48f0eb88a8600d072a72b64faa334b1660caac4b
Edward Seufert <edwardseufert@Edwards-MacBook-Pro.local>
2021-02-12 09:44:13 -0500

    buildout

---
+++ dashboard-view.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+
+export default function DashboardView({servicestats }) {
+
+
+    return (
+      <div>
+        <div> Dashboard Page </div>
+      </div>
+    );
+}
+
+
+DashboardView.propTypes = {
+  servicestats: PropTypes.object
+};
